8cffff6fc9ba82cf102a3c39b51496c22dfe6b01
GPurnachandrika <24B01A4536@svecw.edu.in>
2026-01-13 11:01:14 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,31 +1,12 @@
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-// import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-// import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
-// const firebaseConfig = {
-//   apiKey: "YOUR_API_KEY",
-//   authDomain: "YOUR_PROJECT.firebaseapp.com",
-//   projectId: "YOUR_PROJECT_ID",
-//   storageBucket: "YOUR_PROJECT.appspot.com",
-//   messagingSenderId: "XXXX",
-//   appId: "XXXX"
-// };
-
-// const app = initializeApp(firebaseConfig);
-
-// export const db = getFirestore(app);
-// export const auth = getAuth(app);
-
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDg6pSNikzBhPgvdLILT9rhAZc1XPmJ3PM",
+  apiKey: "YOUR_FIREBASE_API_KEY",
   authDomain: "campusnavigator-d3df5.firebaseapp.com",
   projectId: "campusnavigator-d3df5",
-  storageBucket: "campusnavigator-d3df5.firebasestorage.app",
+  storageBucket: "campusnavigator-d3df5.appspot.com",
   messagingSenderId: "1012224738580",
   appId: "1:1012224738580:web:9ffd1849e02cfb8b612e01",
 };
